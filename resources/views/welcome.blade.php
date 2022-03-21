1d7cff528b9b3e10ed9328c0691b44a52327fdca
Siddhant <sipndy@gmail.com>
2022-03-21 07:36:45 +0530

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,66 @@
+@extends('Layout')
+@section('header')
+<!-- header -->
+<header class="header" style="background-image: url({{asset('images/photography.jpg')}})">
+        <div class="header-text">
+          <h1>BreathTaking Blog</h1>
+          <h4>Dashboard of verified news...</h4>
+        </div>
+        <div class="overlay"></div>
+      </header>
+@endsection
+
+@section('main')
+<main class="container">
+        <h2 class="header-title">Latest Blog Posts</h2>
+        <section class="cards-blog latest-blog">
+          <div class="card-blog-content">
+            <img src="{{asset('images/pic1.jpg')}}" alt="" />
+            <p>
+              2 hours ago
+              <span style="float: right">Written By BreathTaking Wakarindi</span>
+            </p>
+            <h4 style="font-weight: bolder">
+              <a href="single-blog.html"
+                >Benefits of Getting Covid 19 Vaccination</a
+              >
+            </h4>
+          </div>
+
+          <div class="card-blog-content">
+            <img src="{{asset('images/pic2.jpg')}}" alt="" />
+            <p>
+              23 hours ago
+              <span style="float: right">Written By BreathTaking Wakarindi</span>
+            </p>
+            <h4 style="font-weight: bolder">
+              <a href="single-blog.html">Top 10 Music Stories Never Told</a>
+            </h4>
+          </div>
+
+          <div class="card-blog-content">
+            <img src="{{asset('images/pic3.jpg')}}" alt="" />
+            <p>
+              2 days ago
+              <span style="float: right">Written By BreathTaking Wakarindi</span>
+            </p>
+            <h4 style="font-weight: bolder">
+              <a href="single-blog.html"
+                >WRC Safari Rally Back To Kenya After 19 Years</a
+              >
+            </h4>
+          </div>
+
+          <div class="card-blog-content">
+            <img src="{{asset('images/pic4.jpg')}}" alt="" />
+            <p>
+              3 days ago
+              <span style="float: right">Written By BreathTaking Wakarindi</span>
+            </p>
+            <h4 style="font-weight: bolder">
+              <a href="single-blog.html">Premier League 2022/2023 Fixtures</a>
+            </h4>
+          </div>
+        </section>
+      </main>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\WelcomController;
+
+// To welcome page
+Route::get('/', [WelcomController::class, 'index'])->name('welcom.index');
+//to blog page
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+//to single
+Route::get('/blog/post', [BlogController::class, 'show'])->name('blog.show');
+//to about
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+//tocontact
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
